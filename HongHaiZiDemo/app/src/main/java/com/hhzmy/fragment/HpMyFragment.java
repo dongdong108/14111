26b79a6c0993b0dd60dec318399ee4e0c06b58b4
dongdong108 <liudongdong108@163.com>
2016-11-28 09:00:10 +0800

    因为我是你爸爸

---
+++ HongHaiZiDemo/app/src/main/java/com/hhzmy/fragment/HpMyFragment.java
@@ -0,0 +1,23 @@
+package com.hhzmy.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bw.honghaizidemo.R;
+
+/**
+ * Created by asus on 2016/11/8.
+ */
+public class HpMyFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = View.inflate(getActivity(), R.layout.hp_my,null);
+        return view;
+
+    }
+}
